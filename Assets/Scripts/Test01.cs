be578f4dbbb35ec04fe381693e628a725d466ac0
Max <khryshcheniukmaksym@gmail.com>
2022-09-19 22:03:07 +0300

    0.0.0.2.1

--- Assets/Scripts/Test01.cs
+++ Assets/Scripts/Test01.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class Test01 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private void TestDebug(string str) => Debug.Log(str);
 }
